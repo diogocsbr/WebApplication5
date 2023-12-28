ddbde3c00e0de6331f47c3494394364d691be586
Diogo <diogo@codigocentral.com.br>
2023-12-28 20:04:17 -0300

    #1

--- WebApplication5/Controllers/WeatherForecastController.cs
+++ WebApplication5/Controllers/WeatherForecastController.cs
@@ -69,5 +69,18 @@ namespace WebApplication5.Controllers
             })
             .ToArray();
         }
+
+        [HttpGet]
+        [Route("Teste5")]
+        public IEnumerable<WeatherForecast> Get5()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
